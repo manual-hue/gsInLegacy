a8f54809f7e745c69a7b40bd3940d94e232643d3
manual-hue <85495803+manual-hue@users.noreply.github.com>
2022-01-05 16:21:24 +0900

    Update TimeMapper.java

--- src/main/java/org/team/g2/fundboard/mapper/TimeMapper.java
+++ src/main/java/org/team/g2/fundboard/mapper/TimeMapper.java
@@ -1,12 +1,10 @@
 package org.team.g2.fundboard.mapper;
 
-
 import org.apache.ibatis.annotations.Select;
 
 public interface TimeMapper {
 
     @Select("select now()")
     String getTime();
-
 
 }
